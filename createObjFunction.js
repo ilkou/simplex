f4bb07de44b8f8205c2045702e7bb2d1e16f5af9
ilkou <ouklichoussama@gmail.com>
2019-12-15 15:41:00 +0100

    ro

---
+++ createObjFunction.js
@@ -0,0 +1,41 @@
+function createObjFunction(container, num_vars){
+	let title = document.createElement('h1');
+	title.innerHTML = 'Fonction objectif';
+	container.appendChild(title);
+	let table = document.createElement('table');
+	setAttributes(table, {'border': 0, 'cellspacing': 2, 'cellpadding': 2});
+	let tr = document.createElement('tr');
+	let th = document.createElement('th');
+	tr.appendChild(th);
+	for (let i = 1; i <= num_vars; i++) {
+		let th = document.createElement('th');
+		th.innerHTML = 'x' + '<sub>' + i.toString();
+		th.setAttribute('align', 'center');
+		tr.appendChild(th);
+	}
+	table.appendChild(tr);
+	tr = document.createElement('tr');
+	let td = document.createElement('td');
+	let select = document.createElement('select');
+	select.setAttribute('id', 'fct_obj');
+	let option = document.createElement('option');
+	option.innerHTML = 'min';
+	option.setAttribute('value', 'min');
+	select.appendChild(option);
+	option = document.createElement('option');
+	option.innerHTML = 'max';
+	option.setAttribute('value', 'max');
+	select.appendChild(option);
+	td.appendChild(select);
+	tr.appendChild(td);
+	for (let i = 1; i <= num_vars; i++) {
+		let td = document.createElement('td');
+		let input = document.createElement('input');
+		setAttributes(input, {'type': 'text', 'size': '2', 'id': 'x' + i.toString()})
+			setAttributes(td, {'align': 'center'});
+		td.appendChild(input);
+		tr.appendChild(td);
+	}
+	table.appendChild(tr);
+	container.appendChild(table);
+}
---
+++ setArray.js
@@ -0,0 +1,11 @@
+function setArray() {
+	document.getElementById('init').style.display = 'none';
+	let container = document.getElementById('array');
+	if (parseInt(num_vars) > 15 || parseInt(num_conts) > 15)
+		container.removeAttribute("class");
+	createObjFunction(container, num_vars);
+	createContraintes(container, num_vars, num_conts);
+	let input = document.createElement('input');
+	setAttributes(input, {'type':'button', 'value':'Prochain pas →', 'onclick': 'getArray()', 'style': "margin-top: 15px; float: right"})
+		container.appendChild(input);
+}
